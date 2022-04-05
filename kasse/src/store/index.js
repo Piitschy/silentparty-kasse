a30bb86689f4b4d16971393bfa049df721b30c9e
Jan <55839828+Piitschy@users.noreply.github.com>
2022-04-05 22:52:38 +0200

    init

---
+++ kasse/src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+
+Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+  plugins: [vuexLocal.plugin]
+})
